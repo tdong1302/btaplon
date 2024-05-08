0e2c5284b18e0b0a14f785ad350ef6f6f85c2a5a
tdong1302 <dongtv1302@gmail.com>
2024-05-08 11:12:49 +0700

    Create defs.h

---
+++ defs.h
@@ -0,0 +1,17 @@
+#ifndef DEFS_H_INCLUDED
+#define DEFS_H_INCLUDED
+
+const char* WINDOW_TITLE = "FOOT_BALL";
+const int WINDOW_WIDTH = 1200;
+const int WINDOW_HEIGHT = 700;
+const int PLAYER_RADIUS = 20;
+const int FIELD_WIDTH = 1100;
+const int FIELD_HEIGHT = 600;
+const int FIELD_X = (WINDOW_WIDTH - FIELD_WIDTH) / 2;
+const int FIELD_Y = (WINDOW_HEIGHT - FIELD_HEIGHT) / 2;
+const int GOAL_WIDTH = 20; //   chiều rộng Gôn
+const int GOAL_HEIGHT = 160;//  chiều dài Gôn
+const int GOAL_Y = (FIELD_HEIGHT - GOAL_HEIGHT) / 2;  //toạ độ Y của gôn
+
+
+#endif // DEFS_H_INCLUDED
